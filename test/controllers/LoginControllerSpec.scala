d08ae6af5a1b29f87417197d46dc69490364ed49
userjd <2478022+userjd@users.noreply.github.com>
2022-09-28 13:10:20 +0100

    Added tests for login page

--- test/controllers/LoginControllerSpec.scala
+++ test/controllers/LoginControllerSpec.scala
@@ -5,7 +5,7 @@ import org.scalatestplus.play.guice._
 import play.api.mvc.{ AnyContentAsEmpty, DefaultActionBuilder, DefaultMessagesActionBuilderImpl, DefaultMessagesControllerComponents }
 import play.api.test._
 import play.api.test.Helpers._
-import uk.gov.nationalarchives.omega.editorial.controllers.{ LoginController }
+import uk.gov.nationalarchives.omega.editorial.controllers.LoginController
 
 /** Add your spec here.
   * You can mock out a whole application including requests, plugins etc.
@@ -34,7 +34,7 @@ class LoginControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injectin
           stub.executionContext
         )
       )
-      val login = controller.login().apply(FakeRequest(GET, "/login"))
+      val login = controller.view().apply(CSRFTokenHelper.addCSRFToken(FakeRequest(GET, "/login")))
 
       status(login) mustBe OK
       contentType(login) mustBe Some("text/html")
@@ -43,7 +43,7 @@ class LoginControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injectin
 
     "render the login page from the application" in {
       val controller = inject[LoginController]
-      val login = controller.login().apply(FakeRequest(GET, "/login"))
+      val login = controller.view().apply(CSRFTokenHelper.addCSRFToken(FakeRequest(GET, "/login")))
 
       status(login) mustBe OK
       contentType(login) mustBe Some("text/html")
@@ -57,6 +57,60 @@ class LoginControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injectin
       status(login) mustBe OK
       contentType(login) mustBe Some("text/html")
       contentAsString(login) must include("Sign in: Pan-Archival Catalogue")
+    }
+  }
+
+  "LoginController POST" should {
+    "redirect to result page from a new instance of controller" in {
+      val messages: Map[String, Map[String, String]] =
+        Map("en" -> Map("login.heading" -> "Sign in: Pan-Archival Catalogue"))
+      val mockMessagesApi = stubMessagesApi(messages)
+      val stub = stubControllerComponents()
+      val controller = new LoginController(
+        DefaultMessagesControllerComponents(
+          new DefaultMessagesActionBuilderImpl(stubBodyParser(AnyContentAsEmpty), mockMessagesApi)(
+            stub.executionContext
+          ),
+          DefaultActionBuilder(stub.actionBuilder.parser)(stub.executionContext),
+          stub.parsers,
+          mockMessagesApi,
+          stub.langs,
+          stub.fileMimeTypes,
+          stub.executionContext
+        )
+      )
+
+      val login = controller
+        .submit()
+        .apply(
+          CSRFTokenHelper
+            .addCSRFToken(
+              FakeRequest(POST, "/login").withFormUrlEncodedBody("username" -> "1234", "password" -> "1234")
+            )
+        )
+      status(login) mustBe SEE_OTHER
+    }
+
+    "redirect to result page of the application" in {
+      val controller = inject[LoginController]
+      val login = controller
+        .submit()
+        .apply(
+          CSRFTokenHelper.addCSRFToken(
+            FakeRequest(POST, "/login").withFormUrlEncodedBody("username" -> "1234", "password" -> "1234")
+          )
+        )
+
+      status(login) mustBe SEE_OTHER
+    }
+
+    "redirect to result page from the router" in {
+      val request = CSRFTokenHelper.addCSRFToken(
+        FakeRequest(POST, "/login").withFormUrlEncodedBody("username" -> "1234", "password" -> "1234")
+      )
+      val login = route(app, request).get
+
+      status(login) mustBe SEE_OTHER
     }
   }
 }
