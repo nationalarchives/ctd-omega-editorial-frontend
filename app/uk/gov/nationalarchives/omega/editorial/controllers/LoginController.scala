35eb50498faaadd03bb4c337ac8b92c23d78dc18
Rob Walpole <2248695+rwalpole@users.noreply.github.com>
2022-09-29 17:06:40 +0100

    Adding headers and enabling AutomateHeaderPlugin

--- app/uk/gov/nationalarchives/omega/editorial/controllers/LoginController.scala
+++ app/uk/gov/nationalarchives/omega/editorial/controllers/LoginController.scala
@@ -1,3 +1,24 @@
+/*
+ * Copyright (c) 2022 The National Archives
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package uk.gov.nationalarchives.omega.editorial.controllers
 
 import javax.inject._
@@ -12,7 +33,7 @@ import uk.gov.nationalarchives.omega.editorial.models.Credentials
   * application's home page.
   */
 @Singleton
-class LoginController @Inject()(val messagesControllerComponents: MessagesControllerComponents)
+class LoginController @Inject() (val messagesControllerComponents: MessagesControllerComponents)
     extends MessagesAbstractController(messagesControllerComponents) with I18nSupport {
 
   val editorialUsername = scala.util.Properties.envOrElse("CTD_EDITORIAL_USERNAME", "1234")
