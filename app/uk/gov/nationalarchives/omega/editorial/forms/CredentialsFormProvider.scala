4d2ca4c26c85516dacf8e4d6b5c0372a99279ece
userjd <2478022+userjd@users.noreply.github.com>
2022-09-29 17:01:04 +0100

    Added CredentialsFormProvider object for the Login page

---
+++ app/uk/gov/nationalarchives/omega/editorial/forms/CredentialsFormProvider.scala
@@ -0,0 +1,21 @@
+package uk.gov.nationalarchives.omega.editorial.forms
+
+import play.api.data.Form
+import play.api.data.Forms.{ mapping, text }
+import play.api.i18n.{ Lang, MessagesApi }
+import uk.gov.nationalarchives.omega.editorial.models.Credentials
+
+object CredentialsFormProvider {
+
+  val editorialUsername = scala.util.Properties.envOrElse("CTD_EDITORIAL_USERNAME", "1234")
+  val editorialPassword = scala.util.Properties.envOrElse("CTD_EDITORIAL_PASSWORD", "1234")
+
+  def apply()(implicit messagesApi: MessagesApi): Form[Credentials] = Form(
+    mapping(
+      "username" -> text.verifying(messagesApi("login.missing.username")(Lang.apply("en")), !_.isEmpty),
+      "password" -> text.verifying(messagesApi("login.missing.password")(Lang.apply("en")), !_.isEmpty)
+    )(Credentials.apply)(Credentials.unapply)
+      verifying (messagesApi("login.authentication.error")(Lang.apply("en")), credentials =>
+        credentials.username == editorialUsername && credentials.password == editorialPassword)
+  )
+}
