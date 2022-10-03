af750550c7fe50ebfad522dfeafdad2aacebc9e3
userjd <2478022+userjd@users.noreply.github.com>
2022-10-03 11:32:43 +0100

    Update after PR review

--- app/uk/gov/nationalarchives/omega/editorial/forms/CredentialsFormProvider.scala
+++ app/uk/gov/nationalarchives/omega/editorial/forms/CredentialsFormProvider.scala
@@ -7,13 +7,13 @@ import uk.gov.nationalarchives.omega.editorial.models.Credentials
 
 object CredentialsFormProvider {
 
-  val editorialUsername = scala.util.Properties.envOrElse("CTD_EDITORIAL_USERNAME", "1234")
-  val editorialPassword = scala.util.Properties.envOrElse("CTD_EDITORIAL_PASSWORD", "1234")
+  private val editorialUsername = scala.util.Properties.envOrElse("CTD_EDITORIAL_USERNAME", "1234")
+  private val editorialPassword = scala.util.Properties.envOrElse("CTD_EDITORIAL_PASSWORD", "1234")
 
   def apply()(implicit messagesApi: MessagesApi): Form[Credentials] = Form(
     mapping(
-      "username" -> text.verifying(messagesApi("login.missing.username")(Lang.apply("en")), !_.isEmpty),
-      "password" -> text.verifying(messagesApi("login.missing.password")(Lang.apply("en")), !_.isEmpty)
+      "username" -> text.verifying(messagesApi("login.missing.username")(Lang.apply("en")), _.nonEmpty),
+      "password" -> text.verifying(messagesApi("login.missing.password")(Lang.apply("en")), _.nonEmpty)
     )(Credentials.apply)(Credentials.unapply)
       verifying (messagesApi("login.authentication.error")(Lang.apply("en")), credentials =>
         credentials.username == editorialUsername && credentials.password == editorialPassword)
