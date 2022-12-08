5693135b24ff1fed2ae570337a79b21402bbd09b
Sean Rasmussen <tna@erasmos.com>
2022-12-08 16:08:24 +0000

    PACT-570 Minor refactoring.

--- test/support/CustomMatchers.scala
+++ test/support/CustomMatchers.scala
@@ -22,77 +22,56 @@
 package support
 
 import org.jsoup.nodes.Document
-import org.scalatest.matchers.{ MatchResult, Matcher }
+import org.scalatest.matchers.{MatchResult, Matcher}
 
 object CustomMatchers {
 
-  class HaveHeaderTitle(title: String) extends Matcher[Document] {
-
-    override def apply(document: Document): MatchResult = {
-      val actualTitle = document.select("div.govuk-header__content").text()
-      val errorMessageIfExpected =
-        s"The page didn't have a header title of '$title'. The actual title was '$actualTitle'"
-      val errorMessageIfNotExpected = s"The page did indeed have a header title of '$title', which was not expected."
-      MatchResult(
-        actualTitle == title,
-        errorMessageIfExpected,
-        errorMessageIfNotExpected
-      )
-    }
-
+  def haveHeaderTitle: Matcher[Document] = (document: Document) => {
+    val title = "header.title"
+    val actualTitle = document.select("div.govuk-header__content").text()
+    val errorMessageIfExpected =
+      s"The page didn't have a header title of '$title'. The actual title was '$actualTitle'"
+    val errorMessageIfNotExpected = s"The page did indeed have a header title of '$title', which was not expected."
+    MatchResult(
+      actualTitle == title,
+      errorMessageIfExpected,
+      errorMessageIfNotExpected
+    )
   }
 
-  class HaveVisibleLogoutLink() extends Matcher[Document] {
-
-    override def apply(document: Document): MatchResult = {
-      val isVisible = !document.select("div.govuk-header__signout").hasClass("hidden")
-      val errorMessageIfExpected = "We expected the logout link to be visible but it was hidden."
-      val errorMessageIfNotExpected = s"We expected the logout link to be hidden but it was visible."
-      MatchResult(
-        isVisible,
-        errorMessageIfExpected,
-        errorMessageIfNotExpected
-      )
-    }
-
+  def haveVisibleLogoutLink: Matcher[Document] = (document: Document) => {
+    val isVisible = !document.select("div.govuk-header__signout").hasClass("hidden")
+    val errorMessageIfExpected = "We expected the logout link to be visible but it was hidden."
+    val errorMessageIfNotExpected = s"We expected the logout link to be hidden but it was visible."
+    MatchResult(
+      isVisible,
+      errorMessageIfExpected,
+      errorMessageIfNotExpected
+    )
   }
 
-  class HaveLogoutLink(link: String) extends Matcher[Document] {
-
-    override def apply(document: Document): MatchResult = {
-      val actualLink = document.select("div.govuk-header__signout > a").attr("href")
-      val errorMessageIfExpected = s"We expected the sign out link to be '$link' but it was '$actualLink'"
-      val errorMessageIfNotExpected = s"We didn't expect the sign out link to be '$link', but it was."
-      MatchResult(
-        link == actualLink,
-        errorMessageIfExpected,
-        errorMessageIfNotExpected
-      )
-    }
-
+  def haveLogoutLink: Matcher[Document] = (document: Document) => {
+    val link = "/logout"
+    val actualLink = document.select("div.govuk-header__signout > a").attr("href")
+    val errorMessageIfExpected = s"We expected the sign out link to be '$link' but it was '$actualLink'"
+    val errorMessageIfNotExpected = s"We didn't expect the sign out link to be '$link', but it was."
+    MatchResult(
+      link == actualLink,
+      errorMessageIfExpected,
+      errorMessageIfNotExpected
+    )
   }
 
-  class HaveLogoutLinkLabel(label: String) extends Matcher[Document] {
-
-    override def apply(document: Document): MatchResult = {
-      val actualLabel = document.select("div.govuk-header__signout > a").text()
-      val errorMessageIfExpected = s"We expected the logout link label to be '$label' but it was '$actualLabel'"
-      val errorMessageIfNotExpected = s"We didn't expect the logout link label to be '$label', but it was."
-      MatchResult(
-        label == actualLabel,
-        errorMessageIfExpected,
-        errorMessageIfNotExpected
-      )
-    }
-
+  def haveLogoutLinkLabel: Matcher[Document] = (document: Document) => {
+    val label = "header.logout"
+    val actualLabel = document.select("div.govuk-header__signout > a").text()
+    val errorMessageIfExpected = s"We expected the logout link label to be '$label' but it was '$actualLabel'"
+    val errorMessageIfNotExpected = s"We didn't expect the logout link label to be '$label', but it was."
+    MatchResult(
+      label == actualLabel,
+      errorMessageIfExpected,
+      errorMessageIfNotExpected
+    )
   }
-
-  def haveHeaderTitle = new HaveHeaderTitle("header.title")
-
-  def haveVisibleLogoutLink = new HaveVisibleLogoutLink()
-
-  def haveLogoutLink = new HaveLogoutLink("/logout")
-
-  def haveLogoutLinkLabel = new HaveLogoutLinkLabel("header.logout")
 
 }
