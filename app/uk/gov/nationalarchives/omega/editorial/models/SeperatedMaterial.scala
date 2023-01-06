046705a26ae861ce82ecb74db1749e87c0882f78
felixgb <felix@enceladus.software>
2023-01-06 13:08:43 +0000

    PACT-611 Add Sperated material to edit set record

--- app/uk/gov/nationalarchives/omega/editorial/models/SeperatedMaterial.scala
+++ app/uk/gov/nationalarchives/omega/editorial/models/SeperatedMaterial.scala
@@ -23,38 +23,8 @@ package uk.gov.nationalarchives.omega.editorial.models
 
 import play.api.libs.json._
 
-case class EditSetEntry(ccr: String, oci: String, scopeAndContent: String, coveringDates: String)
+case class SeperatedMaterial(linkHref: String, linkText: String, description: Option[String] = None)
 
-case class EditSet(name: String, id: String, entries: Seq[EditSetEntry])
-
-case class EditSetRecord(
-  ccr: String,
-  oci: String,
-  scopeAndContent: String,
-  coveringDates: String,
-  formerReferenceDepartment: String,
-  startDateDay: String,
-  startDateMonth: String,
-  startDateYear: String,
-  endDateDay: String,
-  endDateMonth: String,
-  endDateYear: String,
-  legalStatus: String,
-  placeOfDeposit: String,
-  note: String,
-  relatedMaterial: Seq[RelatedMaterial] = Seq.empty,
-  seperatedMaterial: Seq[SeperatedMaterial] = Seq.empty
-)
-
-object EditSetRecord {
-  implicit val editSetRecordReads = Json.using[Json.WithDefaultValues].reads[EditSetRecord]
-}
-
-object EditSetEntry {
-  implicit val editSetEntryReads = Json.reads[EditSetEntry]
-}
-
-object EditSet {
-  implicit val editSetReads = Json.reads[EditSet]
-
+object SeperatedMaterial {
+  implicit val seperatedMaterialReads = Json.using[Json.WithDefaultValues].reads[SeperatedMaterial]
 }
