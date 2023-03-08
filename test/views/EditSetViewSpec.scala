2f3a508b826d93dbb8e8c44383e6bf66aa58a150
userjd <2478022+userjd@users.noreply.github.com>
2023-03-08 17:21:31 +0000

    PACT-793 Replaced integration tests with unit tests

--- test/views/EditSetViewSpec.scala
+++ test/views/EditSetViewSpec.scala
@@ -22,22 +22,26 @@
 package views
 
 import org.jsoup.nodes.Document
+import play.api.test.Helpers.{ contentType, defaultAwaitTimeout }
 import play.api.test.{ CSRFTokenHelper, FakeRequest, Helpers }
 import play.twirl.api.Html
-import support.BaseSpec
+import support.BaseViewSpec
 import support.CommonMatchers._
 import uk.gov.hmrc.govukfrontend.views.Aliases.Pagination
-import uk.gov.nationalarchives.omega.editorial.models.{ EditSet, EditSetEntry }
-import uk.gov.nationalarchives.omega.editorial.services.EditSetPagination.EditSetPage
+import uk.gov.hmrc.govukfrontend.views.html.components.{ GovukPagination, GovukTable }
+import uk.gov.hmrc.govukfrontend.views.viewmodels.table.Table
+import uk.gov.nationalarchives.omega.editorial.models.{ EditSet, EditSetEntry, User }
 import uk.gov.nationalarchives.omega.editorial.services.EditSetEntryRowOrder
+import uk.gov.nationalarchives.omega.editorial.services.EditSetPagination.EditSetPage
 import uk.gov.nationalarchives.omega.editorial.views.html.editSet
 
-class EditSetViewSpec extends BaseSpec {
+class EditSetViewSpec extends BaseViewSpec {
 
   "Edit set Html" should {
     "render the given title and heading" in {
-
-      val editSetInstance = inject[editSet]
+      val mockGovukPagination = mock[GovukPagination]
+      val mockGovukTable = mock[GovukTable]
+      val editSetInstance = new editSet(mockGovukPagination, mockGovukTable)
       val editSet: EditSet = getEditSetTest("1")
       val title = "EditSetTitleTest"
       val heading = editSet.name
@@ -49,12 +53,17 @@ class EditSetViewSpec extends BaseSpec {
         editSet.entries.size,
         1
       )
+
+      when(mockGovukTable.apply(Table(anyList))).thenReturn(getEditSetTable())
+      when(mockGovukPagination.apply(Pagination())).thenReturn(any)
       val editSetHtml: Html = editSetInstance(user, title, heading, editSetPage, EditSetEntryRowOrder.defaultOrder)(
         Helpers.stubMessages(),
         CSRFTokenHelper.addCSRFToken(FakeRequest())
       )
 
-      val document = asDocument(editSetHtml)
+      contentType(editSetHtml) mustBe "text/html"
+      println(editSetHtml)
+      val document = asDocument(Helpers.contentAsString(editSetHtml))
       document must haveTitle(title)
       document must haveHeader("COAL 80 Sample")
       document must haveCaption("edit-set.table-caption")
@@ -125,7 +134,9 @@ class EditSetViewSpec extends BaseSpec {
     )
 
   private def generateDocument(): Document = {
-    val editSetInstance = inject[editSet]
+    val mockGovukPagination = mock[GovukPagination]
+    val mockGovukTable = mock[GovukTable]
+    val editSetInstance = new editSet(mockGovukPagination, mockGovukTable)
     val editSet: EditSet = getEditSetTest("1")
     val editSetPage = EditSetPage(
       editSet.entries,
@@ -134,17 +145,93 @@ class EditSetViewSpec extends BaseSpec {
       editSet.entries.size,
       1
     )
+    when(mockGovukTable.apply(Table(anyList))).thenReturn(any)
+    when(mockGovukPagination.apply(Pagination())).thenReturn(any)
     asDocument(
-      editSetInstance(
-        user = user,
-        title = "EditSetTitleTest",
-        heading = editSet.name,
-        page = editSetPage,
-        rowOrder = EditSetEntryRowOrder.defaultOrder
-      )(
-        Helpers.stubMessages(),
-        CSRFTokenHelper.addCSRFToken(FakeRequest())
+      Helpers.contentAsString(
+        editSetInstance(
+          user = user,
+          title = "EditSetTitleTest",
+          heading = editSet.name,
+          page = editSetPage,
+          rowOrder = EditSetEntryRowOrder.defaultOrder
+        )(
+          Helpers.stubMessages(),
+          CSRFTokenHelper.addCSRFToken(FakeRequest())
+        )
       )
     )
   }
+
+  private def getEditSetTable(): Html = new Html(
+    """
+      |  <table class="govuk-table">
+      |  
+      |    <caption class="govuk-table__caption govuk-table__caption--m">edit-set.table-caption</caption>
+
+      |    <thead class="govuk-table__head">
+      |     <tr class="govuk-table__row">
+      |       
+      |        <th scope="col" class="govuk-table__header"   aria-sort="ascending">
+      |  
+      |    <a class="govuk-link govuk-link--no-visited-state" href="/edit-set/1?field=ccr&amp;direction=descending">edit-set.record.edit.ccr</a>
+      |  
+      |       </th>
+      |       
+      |         <th scope="col" class="govuk-table__header"  aria-sort="none">
+      |  
+      |    <a class="govuk-link govuk-link--no-visited-state" href="/edit-set/1?field=scope-and-content&amp;direction=ascending">edit-set.record.edit.scope-and-content</a>
+      |  
+      |       </th>
+      |       
+      |        <th scope="col" class="govuk-table__header" aria-sort="none">
+      |  
+      |    <a class="govuk-link govuk-link--no-visited-state" href="/edit-set/1?field=covering-dates&amp;direction=ascending">edit-set.record.edit.covering-dates</a>
+      |  
+      |       </th>
+      |       
+      |     </tr>
+      |    </thead>
+      |  
+      |  <tbody class="govuk-table__body">
+      |    
+      |      <tr class="govuk-table__row">
+      |        
+      |          
+      |         <th scope="row" class="govuk-table__header">
+      |    
+      |        <a class="govuk-link govuk-link--no-visited-state" href=/edit-set/1/record/1/edit>COAL 80/80/1</a>
+      |    
+      |         </th>
+      |            <td class="govuk-table__cell">Bedlington Colliery, Newcastle Upon Tyne. Photograph depicting: view of pithead baths.</td>
+      |            <td class="govuk-table__cell">1960</td>
+      |      </tr>
+      |    
+      |      <tr class="govuk-table__row">
+      |        
+      |          
+      |          <th scope="row" class="govuk-table__header">
+      |    
+      |        <a class="govuk-link govuk-link--no-visited-state" href=/edit-set/1/record/1/edit>COAL 80/80/2</a>
+      |    
+      |         </th>
+      |            <td class="govuk-table__cell">Bedlington Colliery, Newcastle Upon Tyne. Photograph depicting: view of factory.</td>
+      |            <td class="govuk-table__cell">1962</td>
+      |      </tr>
+      |    
+      |      <tr class="govuk-table__row">
+      |         <th scope="row" class="govuk-table__header">
+      |    
+      |        <a class="govuk-link govuk-link--no-visited-state" href=/edit-set/1/record/1/edit>COAL 80/80/3</a>
+      |    
+      |         </th>
+      |            <td class="govuk-table__cell">Bedlington Colliery, Newcastle Upon Tyne. Photograph depicting: view of workers.</td>
+      |            <td class="govuk-table__cell">1964</td>
+      |      </tr>
+      |    
+      |  </tbody>
+      |</table>
+      |
+      |""".stripMargin
+  )
 }
