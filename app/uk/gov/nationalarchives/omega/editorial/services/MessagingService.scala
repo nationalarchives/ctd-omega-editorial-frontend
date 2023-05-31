f8bd0eb26607f1e45c45b6c33649f7fd800ea64f
userjd <2478022+userjd@users.noreply.github.com>
2023-05-31 16:40:24 +0100

    PACT-871 modified to Seq for consistency

--- app/uk/gov/nationalarchives/omega/editorial/services/MessagingService.scala
+++ app/uk/gov/nationalarchives/omega/editorial/services/MessagingService.scala
@@ -20,30 +20,11 @@
  */
 
 package uk.gov.nationalarchives.omega.editorial.services
-/*
- * Copyright (c) 2022 The National Archives
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of
- * this software and associated documentation files (the "Software"), to deal in
- * the Software without restriction, including without limitation the rights to
- * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
- * the Software, and to permit persons to whom the Software is furnished to do so,
- * subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
- * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
- * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
- * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
 
 import cats.effect.IO
 import org.typelevel.log4cats.SelfAwareStructuredLogger
 import org.typelevel.log4cats.slf4j.Slf4jLogger
+import play.api.libs.json.Format.GenericFormat
 import play.api.libs.json.{ Json, Reads }
 import uk.gov.nationalarchives.omega.editorial.connectors.{ ApiConnector, MessageType }
 import uk.gov.nationalarchives.omega.editorial.models.{ AgentSummary, EditSet, EditSetRecord, GetAgentSummaryList, GetEditSet, GetEditSetRecord, GetLegalStatuses, GetPlacesOfDeposit, LegalStatus, PlaceOfDeposit, UpdateEditSetRecord, UpdateResponseStatus }
@@ -89,11 +70,11 @@ class MessagingService @Inject() (apiConnector: ApiConnector) {
         .handle(MessageType.GetPlacesOfDepositType, Json.stringify(Json.toJson(getPlacesOfDeposit)))
         .flatMap(replyMessage => parse[Seq[PlaceOfDeposit]](replyMessage.messageText))
 
-  def getAgentSummaries(getAgentSummaryList: GetAgentSummaryList): IO[List[AgentSummary]] =
+  def getAgentSummaries(getAgentSummaryList: GetAgentSummaryList): IO[Seq[AgentSummary]] =
     logger.info(s"Requesting all of the agent summaries") *>
       apiConnector
         .handle(MessageType.GetAgentSummariesType, Json.stringify(Json.toJson(getAgentSummaryList)))
-        .flatMap(replyMessage => parse[List[AgentSummary]](replyMessage.messageText))
+        .flatMap(replyMessage => parse[Seq[AgentSummary]](replyMessage.messageText))
 
   def getLegalStatuses(getLegalStatuses: GetLegalStatuses): IO[Seq[LegalStatus]] =
     logger.info(s"Requesting all of the legal status summary") *>
